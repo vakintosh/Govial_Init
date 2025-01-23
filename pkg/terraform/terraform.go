c5a7b3a5807b3fea1c750d8dd377661813da7132
Vakintosh <63072787+vakintosh@users.noreply.github.com>
2025-01-23 12:19:30 -0700

    Update terraform.go
    Update code

--- pkg/terraform/terraform.go
+++ pkg/terraform/terraform.go
@@ -16,13 +16,11 @@ func GenerateTerraformProject(outputDir, projectName, author, templateType strin
 		return err
 	}
 
-	// Check Terraform version (v0.13 or later)
 	isV0_13OrLater, err := GetTerraformVersion()
 	if err != nil {
 		return fmt.Errorf("error checking Terraform version: %v", err)
 	}
 
-	// Define template data
 	data := TemplateData{
 		ProjectName:             projectName,
 		Author:                  author,
@@ -30,55 +28,53 @@ func GenerateTerraformProject(outputDir, projectName, author, templateType strin
 		IsTerraformV0_13OrLater: isV0_13OrLater,
 	}
 
-	// Determine the template file based on the templateType
-	var templateFile string
-	if templateType == "module" {
-		templateFile = filepath.Join("pkg", "templates", "terraform", "tf-aws-module.tpl")
-	} else {
-		templateFile = filepath.Join("pkg", "templates", "terraform", "tf-aws.tpl")
-	}
+	templateFile := getTemplateFile(templateType)
 
 	tmpl, err := template.ParseFiles(templateFile)
 	if err != nil {
 		return fmt.Errorf("failed to parse template file: %v", err)
 	}
 
-	// Get the list of files to generate
 	filesToGenerate, err := GetDefinedFiles(templateFile)
 	if err != nil {
 		return fmt.Errorf("failed to extract defined files from template: %v", err)
 	}
 
-	// Use a WaitGroup to synchronize Go routines
-	var wg sync.WaitGroup
-	errChan := make(chan error, len(filesToGenerate)) // Capture errors
+	return generateFiles(outputDir, tmpl, filesToGenerate, data)
+}
 
-	// Generate each file in parallel
+func getTemplateFile(templateType string) string {
+	if templateType == "module" {
+		return filepath.Join("pkg", "templates", "terraform", "tf-aws-module.tpl")
+	}
+	return filepath.Join("pkg", "templates", "terraform", "tf-aws.tpl")
+}
+
+func generateFiles(outputDir string, tmpl *template.Template, filesToGenerate []string, data TemplateData) error {
+	var wg sync.WaitGroup
+	errChan := make(chan error, len(filesToGenerate))
+
 	for _, file := range filesToGenerate {
 		wg.Add(1)
 		go func(file string) {
-			defer wg.Done() // Signal completion of the Go routine
+			defer wg.Done()
 			outputFile := filepath.Join(outputDir, file)
 
-			// Render the template for the current file
 			renderedContent, err := RenderTemplate(tmpl, file, data)
 			if err != nil {
 				errChan <- err
 				return
 			}
 
-			// Create the file and write the content
 			if err := CreateFile(outputFile, renderedContent); err != nil {
 				errChan <- err
 			}
 		}(file)
 	}
 
-	// Wait for all Go routines to complete
 	wg.Wait()
 	close(errChan)
 
-	// Check for errors during file generation
 	for err := range errChan {
 		if err != nil {
 			return err
